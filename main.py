52123e121ff236820a87dd31094f96b070e122cf
Vinicius <vfgouvea@gmail.com>
2023-01-06 13:42:42 -0300

    Publicar

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import functions_framework
-from google.cloud import pubsub_v1
-
-publisher = pubsub_v1.PublisherClient()
+import PubSub
 
 
 @functions_framework.cloud_event
@@ -9,9 +7,4 @@ def start_scraping(cloud_event):
     topic_path = "projects/scraping-3f03b/topics/doScraping"
     product_list = ["Camisa Corinthians", "Boné Vai Corinthians", "Chaveiro Timao"]
     for product_name in product_list:
-        try:
-            future = publisher.publish(topic_path, product_name.encode("utf-8"))
-            print(future.result())
-            print(f"Published {product_name} to {topic_path}.")
-        except Exception as e:
-            print(e)
+        PubSub.publicar(topic_path, product_name)
